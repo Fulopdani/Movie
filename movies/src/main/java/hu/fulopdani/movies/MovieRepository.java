202d9bfabf2fffc009652fdf168c5daf3985717e
Dani <h259372@stud.u-szeed.hu>
2025-11-22 14:57:57 +0100

    Init project

---
+++ movies/src/main/java/hu/fulopdani/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package hu.fulopdani.movies;
+
+import hu.fulopdani.movies.Entity.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
---
+++ movies/src/main/java/hu/fulopdani/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package hu.fulopdani.movies;
+
+import hu.fulopdani.movies.Entity.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
